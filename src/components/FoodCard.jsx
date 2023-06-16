082f67996ecb8d7d45e20bb1ba1602dae1534915
mim2041 <suraiyamim338@gmail.com>
2023-06-16 22:36:26 +0600

    tanstact query used to cart items

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -2,11 +2,13 @@ import { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
 import Swal from "sweetalert2";
 import { useLocation, useNavigate } from "react-router-dom";
+import useCart from "../Hooks/useCart";
 
 
 const FoodCard = ({item}) => {
     const {name, image, price, recipe, _id} = item;
     const {user} = useContext(AuthContext);
+    const [ , refetch] = useCart();
     const navigate = useNavigate();
     const location = useLocation();
     
@@ -25,6 +27,7 @@ const FoodCard = ({item}) => {
             .then(data => {
                 console.log(data);
                 if(data.insertedId){
+                    refetch();  //refetch cart to update the number of items in the cart
                     Swal.fire({
                         position: 'top-end',
                         icon: 'success',
--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -2,9 +2,11 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProvider";
 import { FaShoppingCart } from 'react-icons/fa';
+import useCart from "../../Hooks/useCart";
 
 const Navbar = () => {
     const {user, logOut} = useContext(AuthContext);
+    const [cart] = useCart();
 
     const handleLogOut = () => {
         logOut()
@@ -19,7 +21,7 @@ const Navbar = () => {
         <li><Link to='/'>
             <button className="btn">
                 <FaShoppingCart/>
-                <div className="badge badge-secondary">+99</div>
+                <div className="badge badge-secondary">+{cart?.length}</div>
             </button>
         </Link></li>
         
