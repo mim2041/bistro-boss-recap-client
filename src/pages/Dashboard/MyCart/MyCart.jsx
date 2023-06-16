aab0c43e2e2d0b9193b83f72f12754cb56ee2d3a
mim2041 <suraiyamim338@gmail.com>
2023-06-16 23:47:44 +0600

    cart items shown

--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,5 +1,7 @@
 import { Helmet } from "react-helmet";
 import useCart from "../../../Hooks/useCart";
+import { FaTrashAlt } from 'react-icons/fa';
+
 
 
 const MyCart = () => {
@@ -13,11 +15,50 @@ const MyCart = () => {
             <Helmet>
                 <title>Bistro Boss | My Cart</title>
             </Helmet>
-            <div>
+            <div className="uppercase font-semibold h-[60px] mt-10 flex justify-evenly ">
                 <h2 className="text-3xl">Total Items: {cart.length}</h2>
                 <h2 className="text-3xl">Total Price: ${total}</h2>
                 <button className="btn btn-warning btn-small">Pay</button>
             </div>
+
+            <div className="overflow-x-auto">
+            <table className="table">
+                {/* head */}
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Food</th>
+                    <th>Item Name</th>
+                    <th>Price</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    cart.map((item, index) => <tr
+                        key={item._id}
+                    >
+                        <td>{index + 1}</td>
+                        <td>
+                            <div className="flex items-center space-x-3">
+                                <div className="mask mask-squircle w-12 h-12">
+                                    <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                </div>
+                            </div>
+                        </td>
+                        <td>{item.name}</td>
+                        <td className="text-end">${item.price}</td>
+                        <td>
+                        <button className="btn btn-ghost bg-orange-500 btn-lg"><FaTrashAlt/></button>
+                        </td>
+                    </tr>)
+                }
+                
+                
+                </tbody>
+                
+            </table>
+</div>
         </div>
     );
 };
