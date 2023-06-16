be59ac756bc1519c88bcd6186f1ec1f06e0df8d0
mim2041 <suraiyamim338@gmail.com>
2023-06-16 23:33:10 +0600

    total price added

--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -4,13 +4,20 @@ import useCart from "../../../Hooks/useCart";
 
 const MyCart = () => {
     const [cart] = useCart();
-    
+    // console.log(cart.price);
+    const total = cart.reduce((sum, item) => item.price + sum, 0);
+
+
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | My Cart</title>
             </Helmet>
-            <h2 className="text-3xl">Total Items: {cart.length}</h2>
+            <div>
+                <h2 className="text-3xl">Total Items: {cart.length}</h2>
+                <h2 className="text-3xl">Total Price: ${total}</h2>
+                <button className="btn btn-warning btn-small">Pay</button>
+            </div>
         </div>
     );
 };
