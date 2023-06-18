40cf78354e5e0812bfe1ffe4576897d3220ee275
mim2041 <suraiyamim338@gmail.com>
2023-06-18 22:40:17 +0600

    axios secure interceptor used

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -1,15 +1,28 @@
 import { useQuery } from '@tanstack/react-query'
-import { useContext } from 'react';
-import { AuthContext } from '../Providers/AuthProvider';
+import useAxiosSecure from './useAxiosSecure';
+import useAuth from './useAuth';
 
 const useCart = () => {
-    const {user} = useContext(AuthContext);
+    const {user} = useAuth();
+    // const token = localStorage.getItem('access-token');
+    const [axiosSecure] = useAxiosSecure();
 
     const { refetch, data: cart = [] } = useQuery({
+        // queryKey: ['carts', user?.email],
+        // queryFn: async() => {
+        //     const res = await fetch(`http://localhost:5000/carts?email=${user.email}`, {
+        //         headers: {
+        //             authorization: `bearer ${token}`
+        //         }
+        //     })
+        //     return res.json();
+        // },
+
         queryKey: ['carts', user?.email],
         queryFn: async() => {
-            const res = await fetch(`http://localhost:5000/carts?email=${user.email}`)
-            return res.json();
+            const res = await axiosSecure(`/carts?email=${user.email}`)
+            console.log('res from axios', res)
+            return res.data;
         },
     })
     return [cart, refetch];
