6f4a43fb9b43b71093329eca5423d9c3ef9ae750
mim2041 <suraiyamim338@gmail.com>
2023-06-25 21:04:39 +0600

    pie chart and bar chart drawn

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../Hooks/useAuth";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, PieChart, Pie, Sector, ResponsiveContainer } from 'recharts';
+
 
 
 const AdminHome = () => {
@@ -15,6 +17,33 @@ const AdminHome = () => {
         }
     })
 
+    const {data: chartData = []} = useQuery({
+        queryKey: ['chart-data'],
+        queryFn: async() => {
+            const res = await axiosSecure('/order-stats');
+            return res.data;
+        }
+    })
+
+    const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+        ${x + width / 2}, ${y}
+        C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+        Z`;
+      };
+      
+      const TriangleBar = (props) => {
+        const { fill, x, y, width, height } = props;
+      
+        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+      };
+
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({ innerRadius, outerRadius, }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+
+    }
     return (
         <div className="w-full p-4">
             <h2 className="text-3xl mb-2">Hi, Welcome Back, {user.displayName}</h2>
@@ -57,6 +86,51 @@ const AdminHome = () => {
                 <div className="stat-desc">↘︎ 90 (14%)</div>
             </div>
             
+            </div>
+
+            <div className="flex">
+                <div className="w-1/2">
+                <BarChart
+                    width={500}
+                    height={300}
+                    data={chartData}
+                    margin={{
+                        top: 20,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                    >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="category" />
+                    <YAxis />
+                    <Bar dataKey="total" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                        {chartData.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                        ))}
+                    </Bar>
+                    </BarChart>
+                </div>
+                <div className="w-1/2">
+                <ResponsiveContainer width="100%" height="100%">
+                    <PieChart width={400} height={400}>
+                    <Pie
+                        data={chartData}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={80}
+                        fill="#8884d8"
+                        dataKey="count"
+                    >
+                        {chartData.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+                        ))}
+                    </Pie>
+                    </PieChart>
+                </ResponsiveContainer>
+                </div>
             </div>
         </div>
     );
