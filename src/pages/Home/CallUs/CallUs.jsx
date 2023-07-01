de8994dcef9ac35ec43dcafb39e2616e69558f29
mim2041 <suraiyamim338@gmail.com>
2023-07-01 22:07:22 +0600

    services section added

---
+++ src/pages/Home/CallUs/CallUs.jsx
@@ -0,0 +1,11 @@
+
+
+const CallUs = () => {
+    return (
+        <div className="bg-black text-center">
+            <h3 className="text-3xl text-white py-12 mb-12">Call Us: +88 01353453454</h3>
+        </div>
+    );
+};
+
+export default CallUs;
---
+++ src/pages/Home/Service/Service.jsx
@@ -0,0 +1,15 @@
+// import service from '../../../assets/home/chef-service.jpg';
+import './Service.css'
+
+const Service = () => {
+    return (
+        <div className='service my-12 p-12 '>
+            <div className='inner-service bg-white py-12'>
+                <h1 className='text-5xl uppercase text-center mb-4'>Bistro Boss</h1>
+                <p className='w-4/5 text-center mx-auto'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit maxime enim quam delectus culpa veritatis iste? Sint recusandae expedita ea vitae perferendis quos aspernatur voluptas maxime harum nisi minima facilis, voluptatem exercitationem? Quisquam, totam beatae non officiis tenetur.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
